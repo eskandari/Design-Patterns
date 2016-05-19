69a19c2a7397e1a236cd129cd4b09a1a5ea7f2e2
Ehsan Eskandari <eskandarimashhadrizeie@hotmail.com>
2016-05-18 22:22:25 -0400

    iterator

---
+++ Iterator/IteratorPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new FibonacciSequence(10);
+            foreach (var item in sequence)
+                Console.WriteLine(item);
+        }
+    }
+}
