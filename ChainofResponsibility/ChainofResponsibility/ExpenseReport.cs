ae1efe0a10fbe0247cd76ecb3de2128da53056e2
Ehsan Eskandari <eskandarimashhadrizeie@hotmail.com>
2016-05-19 18:55:16 -0400

    chain of responibility

---
+++ ChainofResponsibility/ChainofResponsibility/ExpenseReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChainofResponsibility
+{
+    public class ExpenseReport : IExpenseReport
+    {
+        public ExpenseReport(Decimal total)
+        {
+            Total = total;
+        }
+
+        public decimal Total
+        {
+            get;
+            private set;
+        }
+    }
+}
