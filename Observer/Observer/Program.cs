5550ece7be557d4e359e9369bc34a044e260f69f
Ehsan Eskandari <eskandarimashhadrizeie@hotmail.com>
2016-05-26 23:20:29 -0400

    Observer

---
+++ Observer/Observer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Observer.IObserver;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockTicker st = new StockTicker();
+
+            GoogleMonitor gf = new GoogleMonitor();
+            MicrosoftMonitor mf = new MicrosoftMonitor();
+
+            using (st.Subscribe(gf))
+            using (st.Subscribe(mf))
+            {
+                foreach (var s in SampleData.getNext())
+                    st.Stock = s;
+            }
+        }
+    }
+}
---
+++ Observer/Observer/Stock.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Observer
+{
+	public class Stock
+	{
+		public string Symbol { get; set; }
+		public decimal Price { get; set; }
+	}
+}
